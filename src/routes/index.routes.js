81c66ed6cd6bd68f5948bbcb180d3ff22fa6fb5f
Jhans-15 <72289225+JFrancodev12@users.noreply.github.com>
2025-12-23 17:18:46 -0500

    fix:server_available

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -6,8 +6,13 @@ import notificationRoutes from './notifications.routes.js'
 import placesRoutes from './places.routes.js';
 import socioRoutes from './socio.routes.js';
 import usersRoutes from './user.routes.js'
+import { ERROR_CODES } from '#src/helpers/errores.js';
 
 const router = Router();
+
+router.get('/', (req, res) => {
+    return res.status(200).json({ok: true, message: 'Servidor disponible', error: '', status: ERROR_CODES.SERVER_AVAILABLE, code: 200})
+})
 
 router.get('/recommendations', getHomeScreenData);
 router.get('/search', searchController)
